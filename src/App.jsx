b39b675d0a7624e339a2da3804b2e7672044968d
Beatriz Dominguez Diaz <isc18350304@gmail.com>
2022-03-31 09:06:05 -0600

    Input

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,61 @@
+import { useState } from "react";
 
 function App() {
-   let varPrueba = "Titulo"
-  const handleChangePrueba = (event) => {  
-    varPrueba=event.target.name;
-  }
+
+  //hooks : Funciones especificas de react todos comienzan con la palabra "use"
+
+  const [inputState,setInputState] = useState({
+    titulo: "",
+    fecha: "",
+    nota: "",
+  }); //VALOR INICIAL DEL STATE
+
+  const handleInputChange = (event) => { 
+    //console.log(event.target);
+      setInputState({
+
+        ...inputState,
+        [event.target.name]: event.target.value,
+    
+    });
+    
+  };
+
   return (
 
     <div className="App">
 
         <h3>pato tieso</h3>
 
-        <label htmlFor="PruebaID">Input de prueba</label>
+        <label htmlFor="titulo"> TITULO UWU </label>
 
       <input 
-      id ="pruebaID" 
-      name="Prueba" 
+      id ="titulo" 
+      name="titulo" 
       type="text"
-      onChange={handleChangePrueba}
-      value={varPrueba}
+      onChange={handleInputChange}
+      value={inputState.titulo}
+      />
+      
+      
+
+      <label htmlFor="fecha"> FECHA </label>
+
+      <input 
+      id ="fecha" 
+      name="fecha" 
+      type="text"
+      onChange={handleInputChange}
+      value={inputState.fecha}
+
+      /><label htmlFor="nota"> NOTA </label>
+
+      <input 
+      id ="nota" 
+      name="nota" 
+      type="text"
+      onChange={handleInputChange}
+      value={inputState.nota}
       />
     </div>
   );
