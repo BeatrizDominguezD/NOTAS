ebcc3a1e36b494658ef5e22f43443578fa1d64db
Beatriz Dominguez Diaz <isc18350304@gmail.com>
2022-04-28 09:56:26 -0500

    Enlistado y Fecha

--- src/App.jsx
+++ src/App.jsx
@@ -11,18 +11,27 @@ function App() {
   }); //VALOR INICIAL DEL STATE
 
   const handleInputChange = (event) => { 
-    //console.log(event.target);
       setInputState({
-
         ...inputState,
         [event.target.name]: event.target.value,
-    
     });
-
   };
 
-  const handleResetClick = ( ) => {
-    setInputState ({titulo: " ", fecha: " ", nota: " "});
+  const handleInputClean = ( ) => {
+    setInputState ({
+      titulo: " ", 
+      fecha: " ", 
+      nota: " "
+    });
+  };
+
+let arregloNotas = JSON.parse (localStorage.getItem ("notas")) || [];
+
+  const handleClickGuardar = ( ) => {
+    let arregloNotas = JSON.parse (localStorage.getItem ("notas")) || [];
+    arregloNotas.push (inputState)
+    localStorage.setItem ("notas",JSON.stringify (arregloNotas));
+    handleInputClean();
   };
     
   return (
@@ -31,60 +40,92 @@ function App() {
       <div className="row">
        <div className= "col">
           <h3> LISTAS </h3>
+          {arregloNotas.length === 0 && 
+            "Al momento no tienes notas guardadas. Puedes crear una en el formulario contiguo"}
+          
+          {arregloNotas.length !== 0 && (
+            <ol>
+              {arregloNotas.map((item) => {
+                return (
+                  <li>
+                    {item.titulo}({item.fecha})
+                  </li>
+                );
+              })}
+            </ol>
+          )}
         </div>
         <div className="col">
-          <h3> NOTAS </h3>
-        <label className = 'mb-2' > 
+        <h3> NOTAS </h3>
+        <label className = 'mb-2' style={{width:"100%"}} > 
 
         TITULO 
 
         <input
-          id ="titulo" 
-          name="titulo" 
+          id = "titulo" 
+          name = "titulo" 
+          type = "text"
+          onChange = {handleInputChange}
+          value = {inputState.titulo}
+          style = {{ width: "100%"}}
+        />
+      </label>
+      <br />
+
+    <label className = "mb-2"
+    style = {{ width: "100%"}}>
+      FECHA
+        <input 
+          id ="fecha" 
+          name="fecha" 
           type="text"
           onChange={handleInputChange}
-          value={inputState.titulo}
-        />
-      <br />
-      </label>
+          value={inputState.fecha}
+          style = {{ width: "100%"}}
+          />
+    </label>
+    <br />
 
-      <label htmlFor="fecha"> FECHA </label>
+    <label className = "mb-2" 
+    style = {{ width: "100%"}}>
+      NOTA
+        <input 
+          id ="nota" 
+          name="nota" 
+          type="text"
+          onChange={handleInputChange}
+          value={inputState.nota}
+          style = {{ width: "100%"}}
+          />
+    </label>
+    <hr />
+      <div className = "mt-2 me-2 mt-2 row">
+        <div className = "col">
+        <span className="row me-1">
+          <button 
+            type = "button" 
+            className ="btn btn-primary"
+            onClick={handleInputClean}
+        >
+            LIMPIAR
+          </button>
+        </span>
+      </div>
+      <div className = "col">
+        <span className = "row ms-1">
+          <button 
+            type="button" 
+            className="btn btn primary"
+            onClick={handleClickGuardar}>
 
-      <input 
-      id ="fecha" 
-      name="fecha" 
-      type="text"
-      onChange={handleInputChange}
-      value={inputState.fecha}
-
-      /><label htmlFor="nota"> NOTA </label>
-
-      <input 
-      id ="nota" 
-      name="nota" 
-      type="text"
-      onChange={handleInputChange}
-      value={inputState.nota}
-      />
-        
+          GUARDAR
+              </button>
+              </span>
+            </div>   
+          </div>
         </div>
       </div>
-      <hr />
-      <div className=" ms-2 mt-2">
-      <button 
-      type = "button" 
-      className ="btn btn-primary"
-      onClick={handleResetClick}
-      style= {{marginLeft: "5px"}}
-      >
-        RESET
-      
-      </button>
-
-    </div>
-    </div>
-
-  );
-}
-
-export default App;
+    </div>     
+    );
+  }
+  export default App;
